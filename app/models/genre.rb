c46746ef39f4b99d483a9d23ce4e97bdf5cf2998
Jul Jen <juliajen.prog@gmail.com>
2018-03-26 18:27:31 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,27 +16,8 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
+    # return an array of strings containing every musician's name
     names = []
     self.artists.map { |artist| artist.name }
-    end
   end
-
 end
-
-
-# def get_genre_of_first_song
-#   #return the genre of the artist's first saved song
-#   self.songs.first.genre
-# end
-#
-# def song_count
-#   #return the number of songs associated with the artist
-#   self.songs.count
-#
-# end
-#
-# def genre_count
-#   #return the number of genres associated with the artist
-#   self.genres.count
-# end
-# end
